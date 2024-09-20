bc1bc3bb50882f848d6e4223c57fa296c52d80f0
Robin Tabard <robin@iconosqua.re>
2024-09-20 14:21:53 +0200

    apply Soren feedback

--- src/utils/helper/getItemsToDisplay/index.ts
+++ src/utils/helper/getItemsToDisplay/index.ts
@@ -3,13 +3,5 @@ export function getItemsToDisplay<T>(
   cursor: number,
   pageItemsNumber: number,
 ): T[] {
-  if (cursor === 0) {
-    return data.slice(0, pageItemsNumber);
-  }
-
-  if (cursor === 1) {
-    return data.slice(pageItemsNumber, pageItemsNumber * 2);
-  }
-
   return data.slice(pageItemsNumber * cursor, pageItemsNumber * (cursor + 1));
 }
